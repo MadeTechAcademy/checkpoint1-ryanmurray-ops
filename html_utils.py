431e7cfbbb644bbb66fcf35594083abd4f059519
Ryan Murray <ryan.murray@madetech.com>
2025-11-24 18:17:11 +0000

    Green: Refactor output to use <ul><li> for duties list

--- html_utils.py
+++ html_utils.py
@@ -12,12 +12,13 @@ def save_duties_to_html(duties, file_path):
             <title>Duties</title>
         </head>
         <body>
+        <ul>
     """)
 
     
     for duty in duties:
-        html_content += f"<p>{duty}</p>\n"
+        html_content += f"<li>{duty}</li>\n"
     
-    html_content += "</body>\n</html>"
+    html_content += "</ul>\n</body>\n</html>"
 
     Path(file_path).write_text(html_content) 
