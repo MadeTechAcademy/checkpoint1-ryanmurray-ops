4756f36f529684939bc19b41977cfe108ef20e11
Ryan Murray <ryan.murray@madetech.com>
2025-11-24 16:58:15 +0000

    Add assertion to test_save_duties_to_html to make it fail

--- tests/test_html_utils.py
+++ tests/test_html_utils.py
@@ -5,3 +5,5 @@ from html_utils import save_duties_to_html
 def test_save_duties_to_html(tmp_path):
     html_file = tmp_path / "duties.html"
     save_duties_to_html(duties, html_file)
+
+    assert html_file.exists()
