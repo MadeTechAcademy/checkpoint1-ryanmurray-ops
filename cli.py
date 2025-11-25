08089adc70599a9fe9c077fac8c54fe025b8636e
Ryan Murray <ryan.murray@madetech.com>
2025-11-25 09:13:36 +0000

    pass: implement get_prompt() to pass prompt text test

--- cli.py
+++ cli.py
@@ -1,5 +1,16 @@
 from themes import list_duties
 
+def get_prompt():
+     return """
+Welcome to apprentice themes!
+
+
+Press (1) to list all the duties
+Press (2) to generate an HTML file of duties
+Enter your choice:
+"""
+
+
 def main():
     x = input("""
     Welcome to apprentice themes!\n
