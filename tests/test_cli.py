cf2fd92c9696e3b114a25c516edcde19b50d9a9d
Ryan Murray <ryan.murray@madetech.com>
2025-11-25 09:06:48 +0000

    add test: create test_cli.py and add prompt text test

---
+++ tests/test_cli.py
@@ -0,0 +1,7 @@
+from cli import get_prompt
+
+def test_prompt_text():
+    prompt = get_prompt()
+    assert "Welcome to apprentice themes!" in prompt
+    assert "Press (1) to list all the duties" in prompt
+    assert "Press (2) to generate an HTML file of duties" in prompt
