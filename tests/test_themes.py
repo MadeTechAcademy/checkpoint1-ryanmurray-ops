2db28605cba95a5807fbb6ec9eb302ef8365d010
Ryan Murray <ryan.murray@madetech.com>
2025-11-26 14:03:51 +0000

    add: test_theme_returns_correct_duty_texts

--- tests/test_themes.py
+++ tests/test_themes.py
@@ -31,3 +31,8 @@ def test_theme_has_correct_name_and_numbers():
     bootcamp = Theme("Bootcamp", [1, 2, 3, 4, 13], duties)
     assert bootcamp.name == "Bootcamp"
     assert bootcamp.duty_numbers == [1, 2, 3, 4, 13]
+
+def test_theme_returns_correct_duty_texts():
+    bootcamp = Theme("Bootcamp", [1, 2, 3, 4, 13], duties)
+    assert len(bootcamp.duties) == 5
+    assert "Script and code in at least one general purpose language" in bootcamp.duties[0]
