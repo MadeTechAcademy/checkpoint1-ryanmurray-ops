3fef3be89176359a5dc71f778966c7ae090839dd
Ryan Murray <ryan.murray@madetech.com>
2025-11-27 14:57:17 +0000

    test: add failing test test_main_print_duties_via_renderer

--- tests/test_renderers.py
+++ tests/test_renderers.py
@@ -8,3 +8,10 @@ def test_standard_renderer_print():
 def test_rich_renderer_print():
     renderer = RichRenderer()
     renderer.print("Hello", style="bold green")
+
+def test_main_prints_duties_via_renderer(capsys):
+    renderer = StandardRenderer()
+    main(renderer=renderer, choice="1")
+    captured = capsys.readouterr
+    assert "Script and code in at least one general purpose language" in captured.out
+    assert "Initiate and facilitate knowledge sharing" in captured out
