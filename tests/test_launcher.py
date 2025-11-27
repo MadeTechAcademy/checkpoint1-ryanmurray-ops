53becc8a17d1c3706775046c69d22e10dabd94ce
Ryan Murray <ryan.murray@madetech.com>
2025-11-27 14:12:13 +0000

    test: add failing test for StandardRenderer selection

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -1,6 +1,11 @@
 from launcher import select_renderer
 from rich_cli import RichRenderer
+from stand_cli import StandardRenderer
 
 def test_select_renderer_returns_rich_renderer():
     renderer = select_renderer(choice="1")
     assert isinstance(renderer, RichRenderer)
+
+def test_select_renderer_returns_standard_renderer():
+    renderer = select_renderer(choice="2")
+    assert isinstance(renderer, StandardRenderer)
