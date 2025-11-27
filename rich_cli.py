1cb267d98087acdf93a011b3fcc05037f9651d29
Ryan Murray <ryan.murray@madetech.com>
2025-11-27 13:59:18 +0000

    pass: create rich_cli.py and create RichRenderer class for styled CLI output

---
+++ rich_cli.py
@@ -0,0 +1,10 @@
+from rich.console import Console
+
+console = Console()
+
+class RichRenderer():
+    def print(self, text, style=None):
+        if style:
+            console.print(text, style=style)
+        else:
+            console.print(text)
