0bcb828a0a70f3d89eb72ebd90266fa163a38356
Ryan Murray <ryan.murray@madetech.com>
2025-11-28 11:49:50 +0000

    test:add failing test tesst_launcher_standard_renderer_prints_main_menu

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -89,6 +89,16 @@ def test_get_main_choice_invalid_then_valid(capsys):
     assert "Invalid choice" in captured.out
     assert choice == 2
 
+def test_launcher_standard_renderer_prints_main_menu(capsys):
+        with patch("builtins.input", return_value="2"):
+            main()
+        
+        captured = capsys.readouterr()
+        assert "Press (1) to list all duties" in captured.out
+        assert "Press (2) to generate an HTML file of duties" in captured.out
+        assert "ress (3) to view duties by theme" in captureed.out
+        
+
 
 
 
